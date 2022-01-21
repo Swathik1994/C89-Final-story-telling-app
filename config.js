c1280ff254b2a7d39d60b64028217e17f2de455e
Swathi WHJ <70898286+Swathik1994@users.noreply.github.com>
2022-01-21 19:56:02 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBx4U4xlVshu6a-wnySw3JR05MLz7iP8og",
+  authDomain: "c-84-e1819.firebaseapp.com",
+  databaseURL: "https://c-84-e1819-default-rtdb.firebaseio.com",
+  projectId: "c-84-e1819",
+  storageBucket: "c-84-e1819.appspot.com",
+  messagingSenderId: "91277138216",
+  appId: "1:91277138216:web:84cfe089ecf2076f0a981d"
+};
